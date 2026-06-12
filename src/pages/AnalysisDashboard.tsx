f037ae9683746e42f35ed61c0eae53a0b4460dc2
kushwahnihal25-rgb <kushwahnihal25@gmail.com>
2026-06-12 12:29:49 +0530

    Move ScanSkeleton to shared component

--- src/pages/AnalysisDashboard.tsx
+++ src/pages/AnalysisDashboard.tsx
@@ -5,6 +5,7 @@ import GlassCard from '../components/GlassCard';
 import StatusTerminal from '../components/StatusTerminal';
 import { api } from '../lib/api';
 import type { ScanResult } from '../lib/types';
+import ScanSkeleton from "../components/shared/ScanSkeleton";
 
 const BIOMARKER_META = {
   gill_saturation: { label: 'Gill Saturation', icon: Droplets },
@@ -19,38 +20,7 @@ function gradeColor(grade: string) {
   if (grade === 'B') return 'text-neon';
   return 'text-error';
 }
-function ScanSkeleton() {
-  return (
-    <div className="p-6 space-y-6 animate-pulse">
-      <div className="flex flex-col md:flex-row gap-6">
-        <GlassCard className="flex-1 p-8">
-          <div className="h-4 w-32 skeleton-shimmer rounded mb-4"></div>
-          <div className="h-16 w-40 skeleton-shimmer rounded mb-4"></div>
-          <div className="h-2 w-full skeleton-shimmer rounded"></div>
-        </GlassCard>
 
-        <GlassCard className="md:w-72 p-6">
-          <div className="h-4 w-24 skeleton-shimmer rounded mb-4"></div>
-          <div className="h-8 w-full skeleton-shimmer rounded mb-3"></div>
-          <div className="h-8 w-full skeleton-shimmer rounded"></div>
-        </GlassCard>
-      </div>
-
-      <GlassCard className="p-6">
-        <div className="h-5 w-40 skeleton-shimmer rounded mb-4"></div>
-        <div className="space-y-3">
-          <div className="h-16 skeleton-shimmer rounded"></div>
-          <div className="h-16 skeleton-shimmer rounded"></div>
-          <div className="h-16 skeleton-shimmer rounded"></div>
-        </div>
-      </GlassCard>
-
-      <GlassCard className="p-4">
-        <div className="h-12 skeleton-shimmer rounded"></div>
-      </GlassCard>
-    </div>
-  );
-}
 export default function AnalysisDashboard() {
  
   const [params] = useSearchParams();
